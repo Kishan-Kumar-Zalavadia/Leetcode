554a17133874c8b5b9ea6c9bdbd046c5962e181b
Kishan Kumar Zalavadia <90181433+Kishan-Kumar-Zalavadia@users.noreply.github.com>
2026-08-01 13:55:43 +0530

    Time: 97 ms (11.27%), Space: 42.9 MB (23.56%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i=0;i<n;i++) {
+            sum+=nums[i];
+        }
+        int p1 = recurssion(nums, 0, n-1);
+        return p1>= (sum-p1);
+    }
+
+    public int recurssion(int[] nums, int s, int e) {
+        if (s>e) return 0;
+        return Math.max(
+            nums[s] + Math.min(
+                recurssion(nums, s+2, e),
+                recurssion(nums, s+1, e-1)
+            ),
+            nums[e] + Math.min(
+                recurssion(nums, s, e-2),
+                recurssion(nums, s+1, e-1)
+            )
+        );
+    }
+}
